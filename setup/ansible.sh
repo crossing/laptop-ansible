69e452728dd75aec525d8b022da6bb3ba6428af3
Xing Yang <xor@jecity.net>
2021-03-02 13:33:48 +0000

    Fix fedora installations

--- setup/ansible.sh
+++ setup/ansible.sh
@@ -4,4 +4,8 @@ mkdir -p $HOME/.local/bin
 export PATH=$PATH:$HOME/.local/bin
 
 /usr/bin/python3 -m pip install --user pipx
-pipx install ansible --include-deps
+
+if ! command -v ansible &> /dev/null
+then
+	pipx install ansible --include-deps
+fi
